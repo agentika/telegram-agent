bae4a0e429d529c7431545bedcd21d61d98f6ca2
narumi <toucans-cutouts0f@icloud.com>
2025-04-29 12:05:21 +0800

    ✨ feat(cli): update run method to accept config file argument

--- src/telegramagent/cli.py
+++ src/telegramagent/cli.py
@@ -1,5 +1,7 @@
 import asyncio
+from typing import Annotated
 
+import typer
 from dotenv import find_dotenv
 from dotenv import load_dotenv
 
@@ -8,14 +10,17 @@ from .bot import TelegramBot
 from .config import Config
 
 
-async def run() -> None:
-    config = Config.from_json("config.json")
+async def arun(config_file: str) -> None:
+    config = Config.from_json(config_file)
     agent = OpenAIAgent.from_config(config)
     async with TelegramBot(agent=agent) as bot:
         await bot.run()
 
 
+def run(config_file: Annotated[str, typer.Option("-c", "--config")] = "config.json") -> None:
+    asyncio.run(arun(config_file))
+
+
 def main() -> None:
     load_dotenv(find_dotenv())
-
-    asyncio.run(run())
+    typer.run(run)
